496bac3446b6e85fb0ceced4764ed90bab0d27cf
unknown <svincent@D201-6.Tertiaire.loges>
2013-11-15 12:15:49 +0100

    Test
    Commentaire de sisi

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main (String [] args){
-		
+		// Sylvain est passé par la !!! ;)
 		Model unModel = new Model (); 
 		Connection connexion = unModel.connexion(); // Chargement du driver et connection a la BDD
 		//unModel.insererContact(3, "Tarrade", "Nicolas");
